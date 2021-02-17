ffbf316a5161a0426982ef16b7255258b776f5c9
Anas Rasheed <76838052+anas-rasheed@users.noreply.github.com>
2021-02-18 02:18:03 +0530

    WIP:handle login route

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const routing = express.Router();
 const setupDB = require('../utilities/setupDB');
-const Users = require('../models/schemas/userSchema');
+const connection = require('../utilities/connection');
 
-// routing.post('/login', function (req, res, next) {
-//   const creds = req.body;
-//   Users.findOne();
-// });
+routing.post('/login', function (req, res, next) {
+  const creds = req.body;
+  connection.getUsersCollection().then((users) => {
+    users
+      .findOne({ username: creds.username, password: creds.password })
+      .then((user) => {
+        if (user) {
+          res.status(201).send('Authenticated');
+        } else {
+          res.status(500).send('Invalid Credentials');
+        }
+      })
+      .catch((err) => next(err));
+  });
+});
 routing.get('/setup', (req, res, next) => {
   setupDB()
     .then((data) => {
